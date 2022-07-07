c751da6225748b26e85111c9f518e902f5f89329
Nachorojo2015 <102544177+Nachorojo2015@users.noreply.github.com>
2022-07-07 18:34:10 -0300

    Add files via upload

---
+++ Frontend/src/pages/NovedadesPage.js
@@ -0,0 +1,50 @@
+const NovedadesPage = (props) => {
+    return (
+       <div>
+         <div class="sub-footer">
+           <div class="sub">
+               <h4>LASTEST NEWS</h4>
+               <br/>
+               <p>The 2019 RDX Will Be the Sportiest Lexus Crossover Ever Built <br/><br/></p>
+               <a href="#">Read more</a>
+               <br/>
+               <br/>
+               <hr/>
+               <br/>
+               <p>Chevrolet is Discontinuing Corvettes for the Next Model Year <br/><br/></p>
+               <a href="#">Read More</a>
+               <br/>
+               <br/>
+               <hr/>
+               <br/>
+               <p>Infiniti Reveals the Completely Redone 2019 QX50 for the US and Europe <br/><br/></p>
+               <a href="#">Read More</a>
+           </div>
+
+           <div class="latest-cars">
+             <h4>LATEST CARS</h4>
+             <div>
+              <img src="Img/azul.jpg" alt=""/>
+              <h5>LAMBORGHINI AVENTADOR</h5>
+              <span class="material-symbols-outlined">add_road</span><p>35,000 KM</p>
+             </div>
+              <hr/>
+              <div>
+              <img src="Img/gris.jpg" alt=""/>
+              <h5>BUGATTI CHIRON</h5>
+              <span class="material-symbols-outlined">add_road</span><p>35,000 KM</p>
+             </div>
+              <hr/>
+              <div>
+              <img src="Img/rojo.jpg" alt=""/>
+              <h5>FERRARI TESTAROSA</h5>
+              <span class="material-symbols-outlined">add_road</span><p>35,000 KM</p>
+              </div>
+           </div>
+        </div>
+       </div>
+
+        );
+    }
+    
+    export default NovedadesPage;
