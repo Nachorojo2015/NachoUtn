e340a37d4722dd1c512da062498d2de1d8780b5b
Nachorojo2015 <102544177+Nachorojo2015@users.noreply.github.com>
2022-08-23 16:04:10 -0300

    Add files via upload

---
+++ Proyecto/frontend/src/components/layout/Header.js
@@ -0,0 +1,14 @@
+const Header = (props) => {
+    return (
+        <div>
+           <header>
+           <div class="logo">
+            <a href=""><img src="https://livedemo00.template-help.com/wt_prod-14633/images/logo.png" alt="Automovil"></img></a>
+           </div>
+           </header>
+        
+        </div>
+    );
+}
+
+export default Header
